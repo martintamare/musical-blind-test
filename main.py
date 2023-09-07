29f0b2cbadf83491658beecf9acbcea186b0c158
Anthony Martinet <anthony@alkivi.fr>
2023-09-07 20:46:21 +0200

    feat(keyboard navigation): basic

--- main.py
+++ main.py
@@ -8,7 +8,13 @@ class App:
         self._padx = 50
         self._pady = 50
 
+        self.buttons = []
+        self.selected_button_index = None
+
+        root.bind('<Up>', lambda event: self.navigate_up())
+        root.bind('<Down>', lambda event: self.navigate_down())
         self.main_frame = None
+
         self.setup_main_frame()
 
     def setup_main_frame(self):
@@ -22,24 +28,16 @@ class App:
 
         self.button1 = Button(root, text="Start Game", **button_dict)
         self.button1.grid(column=0, row=0, **grid_dict)
+        self.buttons.append(self.button1)
 
         self.button2 = Button(root, text="Settings", **button_dict)
         self.button2.grid(column=0, row=1, **grid_dict)
+        self.buttons.append(self.button2)
 
         self.button3 = Button(root, text="Quit", **button_dict)
         self.button3.grid(column=0, row=2, **grid_dict)
+        self.buttons.append(self.button3)
 
-        root.bind("<Configure>", self.on_window_resize)
-
-    def on_window_resize(self, event):
-        width = event.width
-        height = event.height
-        padx = int(width / 4)
-        pady = int(height / 4)
-        self.button1.config(padx=padx, pady=pady)
-        self.button2.config(padx=padx, pady=pady)
-        self.button3.config(padx=padx, pady=pady)
-        print(f"Window resized to {width}x{height} {padx}x{pady}")
 
     def show_settings_screen(self):
         self.main_frame.pack_forget()
@@ -60,16 +58,22 @@ class App:
         frame.pack_forget()
         self.main_frame.pack(fill=BOTH, expand=1)
 
-    def navigate_up(self, event):
-        self.selected_button_index = (self.selected_button_index - 1) % len(
-            self.buttons
-        )
+    def navigate_up(self):
+        if self.selected_button_index is None:
+            self.selected_button_index = 0
+        else:
+            self.selected_button_index = (self.selected_button_index - 1) % len(
+                self.buttons
+            )
         self.update_button_selection()
 
-    def navigate_down(self, event):
-        self.selected_button_index = (self.selected_button_index + 1) % len(
-            self.buttons
-        )
+    def navigate_down(self):
+        if self.selected_button_index is None:
+            self.selected_button_index = 0
+        else:
+            self.selected_button_index = (self.selected_button_index + 1) % len(
+                self.buttons
+            )
         self.update_button_selection()
 
     def select_option(self, event):
@@ -79,6 +83,7 @@ class App:
         for btn in self.buttons:
             btn.config(relief=RAISED)
         self.buttons[self.selected_button_index].config(relief=SUNKEN)
+        self.buttons[self.selected_button_index].focus_set()
 
 
 if __name__ == "__main__":
