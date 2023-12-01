594deb0b7b2333c4a15f1d8adc68b3be59f8811a
Anthony Martinet <anthony@alkivi.fr>
2023-12-01 14:02:16 +0100

    beta version

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Configuration générale du jeu
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 768
 SCREEN_TITLE = "Musical Blind Test"
--- main.py
+++ main.py
@@ -1,28 +1,65 @@
 import config
-from pprint import pprint
 from tkinter import Tk, Frame, Grid, Button
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
-import time, datetime
+import datetime
+from time import sleep
 from playsound import playsound
 
 
-def play_sound(name):
+def play_sound(name, block=False):
     playsound(name, False)
+
+
+class CountDown:
+    def __init__(self, element, actions, time=3):
+        self.element = element
+        self.init_time = time
+        self.time = time
+        self.actions = actions
+        self.element.configure(text=f"{self.time}")
+
+    def run(self):
+        def count():
+            new_time = self.time - 1
+            if new_time < 0:
+                self.element.after_cancel(self.after)
+                for action in self.actions:
+                    action()
+            else:
+                self.time = new_time
+                self.element.configure(text=f"{self.time}")
+                self.after = self.element.after(1000, count)
+
+        count()
+
+    def start(self):
+        self.run()
+
+    def reset(self):
+        self.time = self.init_time
 
 
 class Chronometer:
     def __init__(self, element):
         self.element = element
         self.duration_at_pause = None
+        self.duration = None
+
+    def ok_for_bonus(self):
+        total_seconds = int(self.duration.total_seconds())
+        print(f"ok_for_bonus {total_seconds}")
+        if total_seconds < 4:
+            return True
+        return False
 
     def run(self):
         def count():
-            duration = datetime.datetime.now() - self.start_time
+            self.duration = datetime.datetime.now() - self.start_time
             if self.duration_at_pause:
-                duration += self.duration_at_pause
-            total_seconds = duration.total_seconds()
+                self.duration += self.duration_at_pause
+            total_seconds = self.duration.total_seconds()
             self.element.configure(text=f"{total_seconds:.1f} s")
             self.after = self.element.after(100, count)
 
@@ -43,6 +80,112 @@ class Chronometer:
 
     def resume(self):
         self.start()
+
+
+class SpotifySong:
+    def __init__(self, playlist, uri, name, album, release_date, artist):
+        self.playlist = playlist
+        self.manager = playlist.manager
+        self.sp = playlist.sp
+        self.uri = uri
+        self.name = name
+        self.album = album
+        self.release_date = release_date
+        self.artist = artist
+
+    @property
+    def details(self):
+        return f"{self.artist} - {self.name} - {self.release_date}"
+
+
+class SpotifyPlaylist:
+    def __init__(self, manager, name, uri):
+        self.manager = manager
+        self.uri = uri
+        self.sp = manager.sp
+        self._songs = None
+        self.name = name
+        self.current_index = 0
+        self.player_status = None
+        self.device_id = manager.device_id
+
+    @property
+    def current_song_number(self):
+        return self.current_index + 1
+
+    @property
+    def songs(self):
+        if self._songs is not None:
+            return self._songs
+
+        limit = 50
+        offset = 0
+        stop = False
+        ok_songs = []
+        while not stop:
+            songs = self.sp.playlist_items(
+                self.uri, limit=limit, offset=offset, additional_types=["track"]
+            )
+            items = songs["items"]
+            for item in items:
+                if "track" not in item:
+                    continue
+                track = item["track"]
+                uri = track["uri"]
+                name = track["name"]
+                album = track["album"]
+                release_date = album["release_date"]
+                artists = track["artists"]
+                artist = " & ".join([x["name"] for x in artists])
+                song = SpotifySong(
+                    self,
+                    uri,
+                    name,
+                    album,
+                    release_date,
+                    artist,
+                )
+                ok_songs.append(song)
+            if len(items) == limit:
+                offset += limit
+            else:
+                stop = True
+        self._songs = ok_songs
+        return ok_songs
+
+    @property
+    def number_of_songs(self):
+        return len(self.songs)
+
+    @property
+    def current_song(self):
+        return self.songs[self.current_index]
+
+    @property
+    def is_over(self):
+        next_index = self.current_index + 1
+        if next_index >= self.number_of_songs:
+            return True
+        else:
+            return False
+
+    def next_song(self):
+        self.current_index += 1
+
+    def play(self):
+        uri = self.current_song.uri
+        self.player_status = "play"
+        self.sp.start_playback(device_id=self.device_id, uris=[uri])
+
+    def pause(self):
+        if self.player_status != "pause":
+            self.sp.pause_playback(device_id=self.device_id)
+            self.player_status = "pause"
+
+    def resume(self):
+        if self.player_status != "play":
+            self.sp.start_playback(device_id=self.device_id)
+            self.player_status = "play"
 
 
 class SpotifyManager:
@@ -73,26 +216,6 @@ class SpotifyManager:
             self._sp = spotipy.Spotify(auth_manager=self.auth_manager)
             return self._sp
 
-    def get_playlists(self):
-        limit = 50
-        offset = 0
-        stop = False
-        ok_playlists = {}
-        while not stop:
-            playlists = self.sp.current_user_playlists(limit, offset)
-            items = playlists["items"]
-            for item in items:
-                name = item["name"]
-                if name.lower().startswith("mbt_"):
-                    uri = item["uri"]
-                    ok_playlists[name[4:]] = uri
-
-            if len(items) == limit:
-                offset += limit
-            else:
-                stop = True
-        self._playlists = ok_playlists
-
     def get_device_list(self):
         devices = self.sp.devices()
         self._devices = devices["devices"]
@@ -109,14 +232,43 @@ class SpotifyManager:
 
     @property
     def playlists(self):
-        if self._playlists is None:
-            self.get_playlists()
-        return self._playlists
+        if self._playlists is not None:
+            self._playlists
+
+        limit = 50
+        offset = 0
+        stop = False
+        ok_playlists = []
+        while not stop:
+            playlists = self.sp.current_user_playlists(limit, offset)
+            items = playlists["items"]
+            for item in items:
+                name = item["name"]
+                if name.lower().startswith("mbt_"):
+                    uri = item["uri"]
+                    ok_name = name[4:]
+                    ok_playlists.append(SpotifyPlaylist(self, ok_name, uri))
+
+            if len(items) == limit:
+                offset += limit
+            else:
+                stop = True
+        self._playlists = ok_playlists
+        return ok_playlists
 
     @property
     def devices(self):
-        if self._devices is None:
-            self.get_device_list()
+        if self._devices is not None:
+            return self._devices
+
+        devices = self.sp.devices()
+        self._devices = devices["devices"]
+        for device in self._devices:
+            if device["is_active"]:
+                self.chosen_device = device
+        if self.chosen_device is None:
+            if len(self._devices):
+                self.chosen_device = self._devices[0]
         return self._devices
 
     def check_login_status(self):
@@ -127,17 +279,8 @@ class SpotifyManager:
                 return True
         return False
 
-    def set_playlist(self, name, uri):
-        self.active_playlist = {"name": name, "uri": uri}
-
-    def start_playlist(self):
-        if not self.active_playlist:
-            return
-        uri = self.active_playlist["uri"]
-        items = self.sp.playlist_items(uri)
-        self.active_items = items["items"]
-        self.active_index = 0
-        self.play_track()
+    def set_playlist(self, index):
+        self.active_playlist = self.playlists[index]
 
     @property
     def device_id(self):
@@ -145,16 +288,6 @@ class SpotifyManager:
             return self.chosen_device["id"]
         else:
             return None
-
-    def pause(self):
-        if self.player_status != "pause":
-            self.sp.pause_playback(device_id=self.device_id)
-            self.player_status = "pause"
-
-    def resume(self):
-        if self.player_status != "play":
-            self.sp.start_playback(device_id=self.device_id)
-            self.player_status = "play"
 
     def play_track(self):
         item = self.active_items[self.active_index]
@@ -247,16 +380,21 @@ class Page(Frame):
 
 class GamePage(Page):
     def setup(self):
+        self.active_playlist = self.spotify_manager.active_playlist
         self.buttons = []
         self.player_buttons = []
+        self.score_buttons = []
         self.player_color = {}
 
         for column in range(4):
             self.grid_columnconfigure(column, weight=1)
-        for row in range(4):
+        for row in range(5):
             self.grid_rowconfigure(row, weight=1)
 
-        self.button1 = Button(root, text="Game", **self.button_dict)
+        current_song = self.active_playlist.current_song_number
+        total_songs = self.active_playlist.number_of_songs
+        button_text = f"Song {current_song}/{total_songs}"
+        self.button1 = Button(root, text=button_text, **self.button_dict)
         self.buttons.append(self.button1)
         self.button1.grid(column=0, row=0, **self.grid_dict, columnspan=4)
 
@@ -280,11 +418,17 @@ class GamePage(Page):
             self.player_color[player] = "white"
             index += 1
 
+        for player in range(self.controller.players):
+            score = self.controller.scores[player]
+            button = Button(root, text=f"{score}", **self.button_dict)
+            button.grid(column=player, row=3, **self.player_grid_dict)
+            self.score_buttons.append(button)
+
         self.button3 = Button(root, text="Quit", **self.button_dict)
         self.button3.bind(
             "<Return>", lambda event: self.controller.show_frame("StartPage")
         )
-        self.button3.grid(column=0, row=3, **self.grid_dict, columnspan=4)
+        self.button3.grid(column=0, row=4, **self.grid_dict, columnspan=4)
         self.buttons.append(self.button3)
 
         self.chronometer = Chronometer(self.button2)
@@ -296,19 +440,70 @@ class GamePage(Page):
         self.buttons[0].focus_set()
         self.selected_button_index = 0
         self.chronometer.start()
-        self.spotify_manager.start_playlist()
+        self.active_playlist.play()
+
+    def hide(self):
+        Page.hide(self)
+        if hasattr(self, "player_buttons"):
+            for button in self.player_buttons:
+                button.grid_forget()
+        if hasattr(self, "score_buttons"):
+            for button in self.score_buttons:
+                button.grid_forget()
 
     def pause(self):
         self.chronometer.pause()
-        self.spotify_manager.pause()
+        self.active_playlist.pause()
 
-    def resume(self):
+    def wrong_answer(self):
+        if self.active_playlist.player_status == "play":
+            return
+        play_sound("./sounds/ko.mp3", True)
+        sleep(1)
         self.reset_player_buttons()
-        self.spotify_manager.resume()
+        self.active_playlist.resume()
         self.chronometer.resume()
 
-    def next_song(self):
-        print("TODO")
+    def display_answer(self):
+        button_text = self.active_playlist.current_song.details
+        self.button2.configure(text=button_text)
+
+    def update_scores(self):
+        for index, player in enumerate(self.player_color):
+            color = self.player_color[player]
+            if color == "green":
+                add_score = 1
+                if self.chronometer.ok_for_bonus():
+                    add_score = 2
+                self.controller.scores[index] += add_score
+        for player in range(self.controller.players):
+            score = self.controller.scores[player]
+            print(f"player {player+1} {score}")
+            self.score_buttons[player].configure(text=f"{score}")
+
+    def go_next(self):
+        def next_action():
+            if self.active_playlist.is_over:
+                self.controller.show_frame("ScorePage")
+            else:
+                self.active_playlist.next_song()
+                self.controller.show_frame("SplashPage")
+                self.controller.frames["SplashPage"].reset()
+
+        self.after(5000, next_action)
+
+    def right_answer(self):
+        if self.active_playlist.player_status == "play":
+            return
+        self.active_playlist.resume()
+        self.display_answer()
+        self.update_scores()
+        play_sound("./sounds/ok.mp3", True)
+        self.go_next()
+
+    def skip(self):
+        self.display_answer()
+        self.go_next()
 
     def key_pressed(self, event):
         key = event.char.lower()
@@ -322,13 +517,19 @@ class GamePage(Page):
             player = mapping[key]
             self.answer_from_player(player)
         elif key == "t":
-            self.resume()
+            self.wrong_answer()
 
         elif key == "y":
-            self.next_song()
+            self.right_answer()
+
+        elif key == "h":
+            self.skip()
+
+        elif key == "g":
+            self.go_next()
 
     def answer_from_player(self, player):
-        if self.spotify_manager.player_status == "play":
+        if self.active_playlist.player_status == "play":
             if self.player_color[player] != "red":
                 self.player_color[player] = "green"
                 self.update_player_buttons()
@@ -342,8 +543,33 @@ class GamePage(Page):
     def update_player_buttons(self):
         for player in range(self.controller.players):
             color = self.player_color[player]
-            test = self.player_buttons[player]
             self.player_buttons[player].configure(highlightbackground=color)
+
+
+class SplashPage(Page):
+    def setup(self):
+        self.button1 = Button(root, text="", **self.button_dict)
+        self.buttons.append(self.button1)
+
+        def action_1():
+            self.controller.show_frame("GamePage")
+
+        actions = [action_1]
+
+        self.countdown = CountDown(self.button1, actions, 6)
+        Page.setup(self)
+        self.countdown.start()
+
+    def reset(self):
+        self.countdown.reset()
+        self.countdown.start()
+
+
+class ScorePage(Page):
+    def setup(self):
+        self.button1 = Button(root, text="TODO", **self.button_dict)
+        self.buttons.append(self.button1)
+        Page.setup(self)
 
 
 class StartPage(Page):
@@ -428,7 +654,7 @@ class ReadyPage(Page):
                 ready = False
                 break
         if ready and self.ready:
-            self.controller.show_frame("GamePage")
+            self.controller.show_frame("SplashPage")
 
 
 class PlaylistChoicePage(Page):
@@ -439,14 +665,17 @@ class PlaylistChoicePage(Page):
         self.buttons.append(self.button1)
 
         index = 1
-        for name, uri in self.spotify_manager.playlists.items():
+        for index_playlist, playlist in enumerate(self.spotify_manager.playlists):
             self.grid_rowconfigure(index, weight=1)
             index += 1
 
-            button = Button(root, text=name, **self.button_dict)
+            button_text = f"{playlist.name} - {playlist.number_of_songs} songs"
+            button = Button(root, text=button_text, **self.button_dict)
             button.bind(
                 "<Return>",
-                lambda event, name=name, uri=uri: self.launch_ready_page(name, uri),
+                lambda event, index_playlist=index_playlist: self.launch_ready_page(
+                    index_playlist
+                ),
             )
             self.buttons.append(button)
 
@@ -460,8 +689,8 @@ class PlaylistChoicePage(Page):
         self.buttons.append(self.button4)
         Page.setup(self)
 
-    def launch_ready_page(self, name, uri):
-        self.spotify_manager.set_playlist(name, uri)
+    def launch_ready_page(self, index):
+        self.spotify_manager.set_playlist(index)
         self.controller.show_frame("ReadyPage")
 
 
@@ -503,14 +732,14 @@ class SettingPage(Page):
             self.button2.config(text=f"Logged in as - {name}")
         else:
             self.spotify_manager.login()
-        self.spotify_manager.get_device_list()
+        self.spotify_manager.devices
         if self.spotify_manager.chosen_device:
             device_name = self.spotify_manager.chosen_device["name"]
             button3_text = f"Device : {device_name}"
             self.button3.config(text=button3_text)
 
     def update_device_list(self):
-        devices = self.spotify_manager.get_device_list()
+        self.spotify_manager.devices
         self.controller.show_frame("DevicePage")
 
 
@@ -547,12 +776,14 @@ class PlayerPage(Page):
         if current_players + 1 < 5:
             self.controller.players = current_players + 1
         self.display_players()
+        self.controller.reset_score()
 
     def del_player(self):
         current_players = self.controller.players
         if current_players - 1 > 1:
             self.controller.players = current_players - 1
         self.display_players()
+        self.controller.reset_score()
 
     def display_players(self):
         button1_text = f"Current Player : {self.controller.players}"
@@ -599,7 +830,7 @@ class App:
         self._pady = 50
         self.spotify_manager = SpotifyManager()
         self.players = 2
-        self.scores = {}
+        self.reset_score()
 
         container = Frame(self.root)
         Grid.columnconfigure(root, 0, weight=1)
@@ -621,6 +852,8 @@ class App:
             PlayerPage,
             ReadyPage,
             GamePage,
+            SplashPage,
+            ScorePage,
         ):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
@@ -636,6 +869,12 @@ class App:
         root.bind("<Up>", lambda event: self.navigate_up())
         root.bind("<Down>", lambda event: self.navigate_down())
         root.bind("<KeyPress>", lambda event: self.key_pressed(event))
+
+    def reset_score(self):
+        scores = []
+        for player in range(self.players):
+            scores.append(0)
+        self.scores = scores
 
     def show_frame(self, page_name):
         """Show a frame for the given page name"""
